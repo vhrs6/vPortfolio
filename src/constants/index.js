60ab8393a4ae735f7f974c3672affc773316791a
vhrs6 <115562280+vhrs6@users.noreply.github.com>
2025-07-01 06:58:36 +0530

    Add files via upload

---
+++ src/constants/index.js
@@ -0,0 +1,129 @@
+export const myProjects = [
+  {
+    id: 1,
+    title: "DSCE Exam Results Chatbot",
+    description:
+      "AI-powered chatbot provide real-time exam results via natural language queries.",
+    subDescription: [
+      "Built an AI-powered chatbot using LLMs to provide real-time exam results via natural language queries.",
+    ],
+    href: "https://testdsce.streamlit.app/",
+    logo: "",
+    image: "/assets/projects/dsce.png",
+    tags: [
+      {
+        id: 1,
+        name: "Streamlit",
+        path: "/assets/logos/Streamlit.png",
+      },
+      {
+        id: 2,
+        name: "Python",
+        path: "/assets/logos/py.png",
+      },
+      {
+        id: 3,
+        name: "Sqlite",
+        path: "/assets/logos/sqllite.png",
+      },
+      
+    ],
+  },
+  {
+    id: 2,
+    title: "MCP Client Extension",
+    description:
+      "An MCP client extension allowing users to interaction with MCP servers via both stdio and sse protocols .",
+    subDescription: [
+      "Built an MCP client extension allowing users to select LLM providers using mcp-use, which integrates LangChain adapters and connectors. Enabled interaction with MCP servers via both stdio and sse protocols",
+    ],
+    href: "",
+    logo: "",
+    // image: "/assets/projects/auth-system.jpg",
+    tags: [
+      {
+        id: 1,
+        name: "Python",
+        path: "/assets/logos/py.png",
+      },
+      {
+        id: 2,
+        name: "FastAPI",
+        path: "/assets/logos/fs.png",
+      },
+      // {
+      //   id: 3,
+      //   name: "SQLite",
+      //   path: "/assets/logos/sqlite.svg",
+      // },
+      // {
+      //   id: 4,
+      //   name: "TailwindCSS",
+      //   path: "/assets/logos/tailwindcss.svg",
+      // },
+    ],
+  },
+  {
+    id: 3,
+    title: "Secure Encrypt",
+    description:
+      "Python application that encrypts images by generating a unique hexadecimal key from image pixel data",
+    subDescription: [
+      "Developed a Python application that encrypts images by generating a unique hexadecimal key from image pixel data, enabling secure encryption and decryption",
+    ],
+    href: "https://securencrypt.streamlit.app/",
+    logo: "",
+    image: "/assets/projects/secureencrypt.png",
+    tags: [
+      {
+        id: 1,
+        name: "Python",
+        path: "/assets/logos/py.png",
+      },
+      {
+        id: 2,
+        name: "Streamlit",
+        path: "/assets/logos/Streamlit.png",
+      },
+      
+    ],
+  },
+  
+
+];
+
+export const mySocials = [
+  
+  {
+    name: "Linkedin",
+    href: "https://www.linkedin.com/in/vikhyath-h-r-s-556469330/",
+    icon: "/assets/socials/linkedIn.svg",
+  },
+ 
+];
+
+export const experiences = [
+  {
+    title: "Dyashin Technosoft Pvt. Ltd",
+    job: "Data Analytics Intern",
+    date: "Jan’25 –May’25 ",
+    contents: [
+      "Working on live projects under the mentorship of Satyam Saurav, gaining hands-on experience in relevant technologies.",
+      "Actively participating in daily stand-up meetings, collaborating with teams, and sharing updates.",
+      "Engaging in real-world project execution while ensuring clear communication through daily status reports.",
+    ],
+  },
+  {
+    title: "System Tron Pvt. Ltd",
+    job: "Gen AI Intern",
+    date: "Apr’25 –May’25 ",
+    contents: [
+      "Working on Generative AI projects gaining hands-on experience in prompt engineering, model fine-tuning, and data preprocessing.",
+      "Actively participating in weekly meets, collaborating with teams, and sharing updates",
+      "Maintained clear daily status reports.",
+   ],
+  },
+];
+export const reviews = [
+  
+];
